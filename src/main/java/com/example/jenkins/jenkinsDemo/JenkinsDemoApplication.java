b098d1af79553264460efe1d3f86f79d1ac87f52
dongpeng8 <peng.dong8@pactera.com>
2025-05-20 15:06:25 +0800

    构建

--- src/main/java/com/example/jenkins/jenkinsDemo/JenkinsDemoApplication.java
+++ src/main/java/com/example/jenkins/jenkinsDemo/JenkinsDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsDemoApplication {
 
 	// 入口
-
+	// 构建
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
